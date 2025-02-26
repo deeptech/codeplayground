3cd1e01d24c5a6f5f1deb3c6bf3511c48f173186
deeptech <guptadeepak36@gmail.com>
2025-02-26 17:02:26 +0530

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const haikus = require('./haikus.json');
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {haikus: haikus});
+});
+
+app.listen(port);
+
+module.exports = app; // Export your app for testing
